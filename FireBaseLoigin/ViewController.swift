6430031d606cd2f290d9fbd4179c125ec96ec63e
JEONGHYEONUK <hujeong1004@dgsw.hs.kr>
2018-03-11 22:57:26 +0900

    Initial Commit

---
+++ FireBaseLoigin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireBaseLoigin
+//
+//  Created by JeongHyeonUk on 11/03/2018.
+//  Copyright © 2018 nyup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

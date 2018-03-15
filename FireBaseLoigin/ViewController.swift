39ba880af98e144e89fe227c5278455ac7f9b791
JEONGHYEONUK <hujeong1004@dgsw.hs.kr>
2018-03-15 22:39:05 +0900

    20180315

--- FireBaseLoigin/ViewController.swift
+++ FireBaseLoigin/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, GIDSignInUIDelegate {
+    @IBAction func signIn(_ sender: Any) {
+        GIDSignIn.sharedInstance().signIn()
+    }
+    
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBAction func signin(_ sender: Any) {
+        Auth.auth().createUser(withEmail: email.text!, password: password.text!) { (user, error) in
+            if(error != nil){return}
+            let alert = UIAlertController(title: "알림", message: "회원가입완료", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "확인", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        GIDSignIn.sharedInstance().uiDelegate = self
     }
 
     override func didReceiveMemoryWarning() {
